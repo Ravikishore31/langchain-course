96c536b7bd9f7e7482b674c15e15342333032c52
Ravikishore31 <rkishore.adabala@gmail.com>
2026-09-12 15:10:45 +0530

    Pydantic format added

--- src/langchain_course/react_agent.py
+++ src/langchain_course/react_agent.py
@@ -1,33 +1,33 @@
+from typing import List
 from dotenv import load_dotenv
+from pydantic import BaseModel, Field
 from langchain.agents import create_agent
 from langchain.tools import tool
 from langchain_core.messages import HumanMessage
 from langchain_google_genai import ChatGoogleGenerativeAI
-from tavily import TavilyClient
+from langchain_tavily import TavilySearch
 
 load_dotenv()  # Load environment variables from .env file
 
-tavily = TavilyClient()  # Initialize Tavily client
+class SearchInput(BaseModel):
+    """Schema for the source used by the agent."""
 
-@tool
-def search(query: str):
+    url: str = Field(description="The URL of the source")
 
-    """
-    Tool that searches over internet
-    Args:
-        query: The query to search for
-    Returns:
-        The search results as a string
-    """
-    print(f"Searching for: {query}")
-    return tavily.search(query=query)  # Use Tavily client to perform the search
+class AgentResponse(BaseModel):
+    """Schema for the response returned by the agent."""
 
+    answer: str = Field(description="The agent's response to the user's query")
+    sources: List[SearchInput] = Field(
+        default_factory=list,
+        description="A list of sources used to generate the response"
+    )
 
 llm = ChatGoogleGenerativeAI(
     model="gemini-3.6-flash",
 )
-tools = [search]
-agent = create_agent(model=llm, tools=tools)
+tools = [TavilySearch()]
+agent = create_agent(model=llm, tools=tools, response_format=AgentResponse)
 
 def react_agent():
 
